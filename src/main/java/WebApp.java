c2779131da530726b667da1749434b57678f5195
Gabriel Restori Soares <grsoares21@gmail.com>
2016-10-23 13:45:27 -0200

    First commit with basic project.

---
+++ src/main/java/WebApp.java
@@ -0,0 +1,17 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class WebApp {
+    public static void main(String args[]) throws Exception {
+        Server server = new Server(8080); //initializes a web jetty server in port 8080
+
+        WebAppContext webapp = new WebAppContext();
+        webapp.setResourceBase("src/main/resources/");
+        webapp.setOverrideDescriptor("src/main/resources/webapp/WEB-INF/web.xml");
+        webapp.setContextPath("/"); //set root URL to access this context
+
+        server.setHandler(webapp);
+        server.start();
+        server.join();
+    }
+}
